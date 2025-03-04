23a679c668bb36e9e495c978582be0980a0947af
dphdeetlefs2006 <dphdeetlefs2006@outlook.com>
2025-03-04 17:56:08 +0200

    switched to blendtree for transitioning on player animations

--- Assets/Scripts/Controllers/Player/Player_AC.cs
+++ Assets/Scripts/Controllers/Player/Player_AC.cs
@@ -24,49 +24,50 @@ public class Player_AC : MonoBehaviour
 
     public void SetAnimationForAction(PlayerActionStatus playerActionStatus)
     {
-        switch (playerActionStatus)
-        {
-            case PlayerActionStatus.IDLE:
-                _animator.SetBool("Moving", false);
-                _animator.SetBool("Running", false);
-                break;
-            case PlayerActionStatus.WALK:
-                _animator.SetBool("Moving", true);
-                _animator.SetBool("Running", false);
-                break;
-            case PlayerActionStatus.JUMP:
-                break;
-            case PlayerActionStatus.WALL_JUMP:
-                break;
-            case PlayerActionStatus.FALL:
-                break;
-            case PlayerActionStatus.LAND:
-                break;
-            case PlayerActionStatus.DASH:
-                break;
-            case PlayerActionStatus.ATTACK:
-                break;
-            case PlayerActionStatus.HURT:
-                break;
-            case PlayerActionStatus.DEAD:
-                break;
-            case PlayerActionStatus.CROUCH:
-                break;
-            case PlayerActionStatus.CRAWL:
-                break;
-            case PlayerActionStatus.ROLL:
-                break;
-            case PlayerActionStatus.CLIMB:
-                break;
-            case PlayerActionStatus.LEDGE_GRAB:
-                break;
-            case PlayerActionStatus.RUN:
-                _animator.SetBool("Moving", false);
-                _animator.SetBool("Running", true);
-                break;
-            default:
-                break;
-        }
+        _animator.SetFloat("MovementAction", (float)playerActionStatus);
+        //switch (playerActionStatus)
+        //{
+        //    case PlayerActionStatus.IDLE:
+        //        //_animator.SetBool("Moving", false);
+        //        //_animator.SetBool("Running", false);
+        //        break;
+        //    case PlayerActionStatus.WALK:
+        //        //_animator.SetInteger("Moving", true);
+        //        //_animator.SetBool("Running", false);
+        //        break;
+        //    case PlayerActionStatus.JUMP:
+        //        break;
+        //    case PlayerActionStatus.WALL_JUMP:
+        //        break;
+        //    case PlayerActionStatus.FALL:
+        //        break;
+        //    case PlayerActionStatus.LAND:
+        //        break;
+        //    case PlayerActionStatus.DASH:
+        //        break;
+        //    case PlayerActionStatus.ATTACK:
+        //        break;
+        //    case PlayerActionStatus.HURT:
+        //        break;
+        //    case PlayerActionStatus.DEAD:
+        //        break;
+        //    case PlayerActionStatus.CROUCH:
+        //        break;
+        //    case PlayerActionStatus.CRAWL:
+        //        break;
+        //    case PlayerActionStatus.ROLL:
+        //        break;
+        //    case PlayerActionStatus.CLIMB:
+        //        break;
+        //    case PlayerActionStatus.LEDGE_GRAB:
+        //        break;
+        //    case PlayerActionStatus.RUN:
+        //        _animator.SetBool("Moving", false);
+        //        _animator.SetBool("Running", true);
+        //        break;
+        //    default:
+        //        break;
+        //}
     }
 
 }
