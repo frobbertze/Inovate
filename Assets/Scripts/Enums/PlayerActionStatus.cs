c51a7936c5a27a2035711d5fba40c7e22e7ea49b
Freddie Deetlefs <freddie.deetlefs@outlook.com>
2025-03-04 09:25:24 +0200

    commit PlayerActionStatus

---
+++ Assets/Scripts/Enums/PlayerActionStatus.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public enum PlayerActionStatus
+{
+    IDLE = 0,
+    WALK = 1,
+    JUMP = 2,
+    WALL_JUMP = 3,
+    FALL = 4,
+    LAND = 5,
+    DASH = 6,
+    ATTACK = 7,
+    HURT = 8,
+    DEAD = 9,
+    CROUCH = 10,
+    CRAWL = 11,
+    ROLL = 12,
+    CLIMB = 13,
+    LEDGE_GRAB = 14,
+    RUN = 15
+}
